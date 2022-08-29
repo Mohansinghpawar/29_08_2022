31add8d470afb21765957771130478b0e005147f
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-08-29 17:24:16 +0530

    Create MemoryCasting.java

---
+++ MemoryCasting.java
@@ -0,0 +1,12 @@
+public class MemoryCasting{	 
+	public static void main(String[] args) {
+		double sizeInBytes=99621514731588D;
+		String size = null;
+		double size_kb = size_bytes /1024;
+		double size_mb = size_kb / 1024;
+		double size_gb = size_mb / 1024 ;
+	    System.out.println(cnt_size = size_gb + " GB");
+	    System.out.println(cnt_size = size_mb + " MB");
+	    System.out.println(cnt_size = size_kb + " KB");
+	}
+}
